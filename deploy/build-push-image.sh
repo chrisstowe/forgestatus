89e72c52db8578bf5010d5beccef5c719d8b9d32
Chris Stowe <chstowe@gmail.com>
2019-02-10 14:44:02 -0800

    Remove image nesting

--- deploy/build-push-image.sh
+++ deploy/build-push-image.sh
@@ -6,7 +6,7 @@ echo ${GCP_SERVICE_ACCOUNT} > gcp-service-account.json
 
 gcloud auth activate-service-account --key-file gcp-service-account.json
 
-DOCKER_NAME=gcr.io/forgestatus/${CIRCLE_PROJECT_REPONAME}/${SERVICE}
+DOCKER_NAME=gcr.io/forgestatus/${CIRCLE_PROJECT_REPONAME}-${SERVICE}
 
 gcloud docker -- build -t ${DOCKER_NAME}:${CIRCLE_SHA1} -f ${SERVICE}.Dockerfile .
 
