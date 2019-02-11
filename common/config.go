163525cd3642634e736f49ae5a4ab4c115b6cf8c
Chris Stowe <chstowe@gmail.com>
2019-02-11 12:56:53 -0800

    Add redis url to the config

--- common/config.go
+++ common/config.go
@@ -5,11 +5,13 @@ import "os"
 // Config holds required and optional config parameters.
 type Config struct {
 	Port,
+	RedisURL,
 	WorkerID string
 }
 
 // EnvConfig holds the current environment configs.
 var EnvConfig = Config{
 	Port:     os.Getenv("PORT"),
+	RedisURL: os.Getenv("REDIS_URL"),
 	WorkerID: os.Getenv("WORKER_ID"),
 }
