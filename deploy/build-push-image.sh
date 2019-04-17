4f67b728c70c77448c61dfe9217a92f468f342cb
Chris Stowe <chstowe@gmail.com>
2019-04-17 08:05:21 -0700

    Remove tag

--- deploy/build-push-image.sh
+++ deploy/build-push-image.sh
@@ -11,14 +11,3 @@ DOCKER_NAME=gcr.io/forgestatus/${CIRCLE_PROJECT_REPONAME}-${SERVICE}
 gcloud docker -- build -t ${DOCKER_NAME}:${CIRCLE_SHA1} -f ${SERVICE}.Dockerfile .
 
 gcloud docker -- push ${DOCKER_NAME}:${CIRCLE_SHA1}
-
-if [ ${CIRCLE_BRANCH} = 'master' ]; then
-    gcloud container images add-tag ${DOCKER_NAME}:${CIRCLE_SHA1} \
-        ${DOCKER_NAME}latest
-else
-    gcloud container images add-tag ${DOCKER_NAME}:${CIRCLE_SHA1} \
-        ${DOCKER_NAME}${CIRCLE_SHA1}-pr-`echo ${CIRCLE_PULL_REQUEST} | grep -om1 '[0-9]\+$'`
-fi
-
-gcloud container images add-tag ${DOCKER_NAME}:${CIRCLE_SHA1} \
-    ${DOCKER_NAME}:${CIRCLE_SHA1}-branch-`echo ${CIRCLE_BRANCH} | sed -e 's/[^a-zA-Z0-9\-]/-/g'`
